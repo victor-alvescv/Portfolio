cc699533300989e2947c4a9401fd54c9db1fdee2
Victor Alves <victoralvescabral7@gmail.com>
2024-03-06 17:54:39 -0300

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,147 @@
+import { useState, useEffect } from "react"
+import "./style.css"
+import BackgroundLines from "../BackgroundLines"
+import Button from "../Button"
+import MenuIcon from "../../assets/Icon/menu.svg"
+import ArrowUpRightIcon from "../../assets/Icon/arrow-up-right.svg"
+import headerVideo from "../../assets/headerVideo.webm"
+import TextWriting from "../TextWriting"
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+import ScrambleText from "../ScrambleText"
+import Time from "../Time"
+import NavMenu from "../NavMenu"
+
+export default function Header() {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+  const [hasAnimated, setHasAnimated] = useState(false)
+  const [menuVisible, setMenuVisible] = useState(false)
+
+  const handleComplete = () => {
+    setHasAnimated(true)
+  }
+
+  useEffect(() => {
+    // Start animation when the component is in view
+    if (inView && !hasAnimated) {
+      controls.start("visible")
+    }
+  }, [inView, controls])
+
+  const nameVariants = {
+    hidden: { scale: 0 },
+    visible: {
+      scale: 1,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        duration: 1.5,
+        delay: 2.85,
+      },
+    },
+  }
+
+  const opacityVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
+
+  const blurVariants = {
+    hidden: { opacity: 0, filter: "blur(10px)" },
+    visible: { opacity: 1, filter: "blur(0px)" },
+  }
+
+  const handleMenuToggle = () => {
+    setMenuVisible((prev) => !prev)
+  }
+
+  return (
+    <header ref={ref}>
+      <NavMenu isVisible={menuVisible} toggleFunc={handleMenuToggle} />
+      <BackgroundLines />
+      <NavMenu />
+
+      <motion.div initial="hidden" animate="visible" variants={blurVariants} transition={{ duration: 1, delay: 0.5 }} className="header--menuBtn">
+        <Button icon={MenuIcon} onClick={handleMenuToggle} />
+      </motion.div>
+
+      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 0.5 }} className="header--top">
+        <h3>
+          <a href="https://github.com/victor-alvescv" target={"_blank"} rel="noopener noreferrer">
+            <ScrambleText shuffle delay={0.5}>
+              GITHUB
+            </ScrambleText>
+          </a>
+          <span className="header--hash">{"//"}</span>
+          <a href="https://www.linkedin.com/in/victor-alves-68538527b/" target={"_blank"} rel="noopener noreferrer">
+            <ScrambleText shuffle delay={1}>
+              LINKEDIN
+            </ScrambleText>
+          </a>
+          <span className="header--hash">{"//"}</span>
+          <a href="https://docs.google.com/document/d/1eoDcsKkNFYl0iHpXfbQe3CuV34wxfxHQKoJoktNOK4g/edit?usp=sharing" target={"_blank"} rel="noopener noreferrer">
+            <ScrambleText shuffle delay={1.5}>
+              RESUME
+            </ScrambleText>
+          </a>
+        </h3>
+      </motion.div>
+
+      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 4 }} className="header--bottom">
+        <div>
+          <h3>
+            <ScrambleText shuffle delay={4}>
+              intro
+            </ScrambleText>{" "}
+            <span className="header--hash">{"//"}</span>
+          </h3>
+          <p className="theme--detail">
+            <ScrambleText shuffle delay={4}>
+              Hey there, I'm Victor, I tackle complex engineering challenges daily, alongside a team of elite software engineers. My mission is to continuously advance in web development, using my growing expertise to drive societal progress through technology.
+            </ScrambleText>
+          </p>
+        </div>
+
+        <h3>
+          <Time delay={4.0} />
+        </h3>
+      </motion.div>
+
+      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 4.5 }} className="header--center" onAnimationComplete={() => handleComplete()}>
+        <a href="#contact" className="connect--button">
+          <Button label="Let’s connect" icon={ArrowUpRightIcon} />
+        </a>
+      </motion.div>
+
+      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 2.85 }} className="header--right">
+        <h3>
+          <span className="header--hash">{"//"}</span>{" "}
+          <ScrambleText shuffle delay={2.9}>
+            scroll
+          </ScrambleText>{" "}
+          <span className="header--hash">{"//"}</span>
+        </h3>
+      </motion.div>
+
+      <motion.div initial="hidden" animate={controls} variants={opacityVariants} transition={{ duration: 2, delay: 2.85 }} className="header--video">
+        <video src={headerVideo} autoPlay loop muted></video>
+      </motion.div>
+
+      <h1 className="header--name">
+        <TextWriting controls={controls} text={"Victor Alves"} noblink />
+        <br />
+        <TextWriting controls={controls} delay={1.65} text={"Frontend"} noblink />{" "}
+        <motion.div initial="hidden" animate={controls} variants={nameVariants} className="header--name--sec">
+          <TextWriting controls={controls} delay={2.85} text={"Developer"} noblink />
+          <div className="header--name--border">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </motion.div>
+      </h1>
+    </header>
+  )
+}
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,125 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+import BackgroundLines from "../BackgroundLines";
+import WorkCard from "../WorkCard";
+import ScrambleText from "../ScrambleText";
+import ParaWriting from "../ParaWriting";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import work1 from "../../assets/Images/work1.png";
+import work2 from "../../assets/Images/work2.png";
+import work3 from "../../assets/Images/work3.png";
+
+export default function Projects() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  const handleComplete = () => {
+    setHasAnimated(true);
+  };
+
+  useEffect(() => {
+    // Start animation when the component is in view
+    if (inView && !hasAnimated) {
+      controls.start("visible");
+    }
+  }, [inView, controls]);
+
+  const works = [
+    {
+      client: "Frontend Simplified",
+      year: "2023",
+      img: work1,
+      title: "Building from the ground up a book store",
+      detail:
+        "Working at a startup brought the opportunity to bring many projects from their early days of development to a fully fledged application, I've had the opportunity to be a core part of multiple projects at preezie.",
+    },
+    {
+      client: "Frontend Simplified",
+      year: "2022",
+      img: work2,
+      title: "Creating the future as a shop",
+      detail:
+        "Empowering students with the knowledge and skills they need to succeed in the dynamic field of programming. Having tutored 100's of students within the Frontend simplified curriculum, this gives me a great inspiration to lead.",
+    },
+    {
+      client: "Prifina",
+      year: "2023",
+      img: work3,
+      title: "Information, weather and health",
+      detail:
+        "Considered the best project ever seen from the company, Airwise was created by 4 people in the period of three months without one single dollar. A platform capable of giving so many information about your health and weather. It shows my abilitys at working as a team in such a big project with that high quality, inspirating me to keep going and believe in myself and in my work!",
+    },
+  ];
+
+  const opacityVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <section ref={ref} className="projects" id="projects">
+      <BackgroundLines />
+      <div className="background--glow"></div>
+
+      <div className="projects--grid">
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={opacityVariant}
+          transition={{ duration: 1, delay: 0.5 }}
+          className="projects--grid--title"
+        >
+          <h3 className="theme--text">
+            <ScrambleText shuffle delay={0.5}>
+              03
+            </ScrambleText>{" "}
+            <span className="hash">{"//"}</span>{" "}
+            <ScrambleText shuffle delay={0.5}>
+              Expertise
+            </ScrambleText>
+          </h3>
+        </motion.div>
+
+        <div className="projects--grid--content">
+          <div className="projects--grid--content--heading">
+            <h2>
+              <ParaWriting stagger={0.08} text={"My "} sec={"Works"} />
+            </h2>
+          </div>
+          <div className="projects--grid--content--works">
+            {works.map((item, index) => {
+              return (
+                <WorkCard
+                  item={item}
+                  key={index}
+                  // delay={0.1 * index + 1}
+                  // controls={controls}
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={opacityVariant}
+          transition={{ duration: 1, delay: 1 }}
+          onAnimationComplete={() => handleComplete()}
+          className="projects--grid--detail"
+        >
+          <p className="theme--detail">
+            <ScrambleText delay={1}>
+              Discover a curated portfolio of projects where each line of code
+              tells a story of problem-solving, creativity, and technical
+              finesse.
+            </ScrambleText>
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+}
